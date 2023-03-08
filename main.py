6d4d98ff27ebea4de1cb10eba990618c07dfc0d4
Isaiah Tanon <Isaiahtanon@ufl.edu>
2023-03-08 15:56:08 -0500

    Updated

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def encode(password):
+    x = str(password)
+    final_list = []
+    for i in range(len(x)):
+        y = int(x[i])
+        final_list += [str(y + 3)]
+    encoded_password = ''.join(final_list)
+    return 'Your password has been encoded and stored!'
+
 def main():
     print('Menu\n'
           '-------------\n'
@@ -6,13 +15,7 @@ def main():
           '3. Quit')
     choice = input('Please enter an option:')
     if choice == '1':
-        x = str(input('Please enter your password to encode:'))
-        final_list = []
-        for i in range(len(x)):
-            y = int(x[i])
-            final_list += [str(y + 3)]
-        encoded_password = ''.join(final_list)
-        return 'Your password has been encoded and stored!'
+        print(encode(input('Please enter your password to encode:')))
 
-print(main())
+main()
 
